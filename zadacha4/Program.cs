c7728d7a37e74022d0c9365aaefbc69789447c5e
Elvira <6290548@tut.by>
2023-11-27 01:55:16 +0300

    nomer4

---
+++ zadacha4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задание 4
+Console.Write("Введите любое натуральное число: ");
+int number = int.Parse(Console.ReadLine()!);
+int i = 0;
+while (i <= number)
+{
+Console.Write($"{i},");
+  i++;
+}
+
