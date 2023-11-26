359d6d7ff58b05537f1cd2e88015ca213f04fe8e
Elvira <6290548@tut.by>
2023-11-26 03:04:56 +0300

    задача1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задание 1
+Console.Clear ();
+Console.Write("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 7 == 0 && a % 23 == 0)
+
+Console.WriteLine("Число кратно");
+
+else
+
+Console.WriteLine("Число не кратно");
